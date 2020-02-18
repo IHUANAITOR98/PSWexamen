d61ec34081af229efcc8719d795c1461a8b39724
IHUANAITOR98 <saidsongarcel@outlook.com>
2020-02-18 08:15:25 -0600

    Curriculum:1

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Said C.V
+	</title>
+	<link rel="stylesheet" 
+	type="text/css" 
+	href="css/estilo.css">
+</head>
+<body>
+	<nav>
+		Curriculum Vitae
+	</nav>
+	<div class="Principal">
+		<article >
+			<div>
+				<hr>	
+				<section class="descripcion">
+						Estudiante a nivel medio superior con intereses en el campo laboral de Tecnologías de la Información
+
+				</section>
+				<hr>
+				<section class="contacto">
+					Formas de contacto:
+					<ul type="circle">
+						<li> Correo: <a href=" mailto: agarciap1802@alumno.ipn.mx">agarciap1802@alumno.ipn.mx</a></li>
+						<li>Teléfono: 5617502128</li>
+					</ul>
+				</section>
+				<hr>
+				<section class="animacion"></section>
+				<hr>
+			</div>
+		</article>
+		<style type="text/css">
+			.vl{
+				border-left: 8px solid white;
+				height: 350px;
+			}
+		</style>
+		<div class="vl"></div>
+		<aside class="información">
+			<table  class="tabla" 
+			cellpadding="4">
+				<tr>
+					<td colspan="3"> Nombre: </td>
+				</tr>
+				<tr>
+					<td colspan="3"> Ángel Said García Piedra </td>
+					
+				</tr>
+				<tr>
+					<td> Edad: </td>
+					<td> Fecha de nacimiento: </td>
+					<td> Sexo: </td>
+				</tr>
+				<tr>
+					<td> 16 </td>
+					<td> 9 de agosto del 2003 </td>
+					<td> Masculino </td>
+				</tr>
+				<tr>
+					<td colspan="3"> Nivel de Estudios cursados: </td>
+				</tr>
+				<tr>
+					<td rowspan="2" colspan="3"> Tercer Semestre de Bachillerato Tecnológico (Actualmente) </td>
+				</tr>
+				<tr></tr>
+				<tr>
+					<td colspan="3"> Conocimientos: </td>
+				</tr>
+				<tr>
+					<td colspan="3"> <ul type="square"> 
+										<li> Lenguaje de programación Java</li>
+										<li> Lenguaje de programación Python</li>
+										<li> HTML</li>
+										<li> CSS</li>
+									</ul> 
+					</td>
+				</tr>
+				<tr>
+					<td colspan="3"> Logros: </td>
+				</tr>
+				<tr>
+					<td colspan="3"> 
+						<ul type="square">
+							<li> Haber sido asignado en el examen COMIPEMS a mi primera opción(2018)</li>
+							<li> Medalla de Bronce en Nado Estilo Libre en la etapa de competencia Municipal de Tecámac</li>
+							<li>
+								Seleccionado de la ronda Regional para la XI Olimpiada de Historia
+							</li>
+						</ul>
+					</td>
+				</tr>
+			</table>
+		</aside>
+	</div>
+</body>
+</html>
